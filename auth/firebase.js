54fe2cef70204d0d9012e0366db24b53f7b43915
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2021-11-13 11:49:30 -0600

    test

--- auth/firebase.js
+++ auth/firebase.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
