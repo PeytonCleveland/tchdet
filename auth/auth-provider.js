13455f9586983605924124ef3c7d5b2e9888a2b6
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2021-11-13 19:05:07 -0600

    auth fixes

--- auth/auth-provider.js
+++ auth/auth-provider.js
@@ -7,19 +7,20 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 const AuthProvider = ({ children }) => {
   const router = useRouter();
   const [user, loading, error] = useAuthState(auth);
+  const path = router.pathname;
 
   useEffect(() => {
     if (!loading) {
-      if (user == null && router.pathname.includes('/app')) {
-        console.log('redirecting from ', router.pathname, ' to /sign-in');
-        router.push('/sign-in');
+      if (user == null && path.includes('/app')) {
+        console.log('redirecting from ', path, ' to /sign-in');
+        router.replace('/sign-in');
       }
-      if (user != null && !router.pathname.includes('/app')) {
+      if (user != null && !path.includes('/app')) {
         console.log('redirecting to /app');
         router.push('/app');
       }
     }
-  }, [user, loading]);
+  }, [user, loading, path]);
 
   return loading ? null : (
     <UserContext.Provider value={{ user: user }}>
